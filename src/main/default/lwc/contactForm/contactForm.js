4d9f6c4e110cc9a6211a91f369159529f08e5928
Matthew Doles <matteddied@gmail.com>
2020-11-26 10:54:34 -0600

    Handle Contact Form submissions, send myself email notification

--- src/main/default/lwc/contactForm/contactForm.js
+++ src/main/default/lwc/contactForm/contactForm.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class ContactForm extends LightningElement {}
+export default class ContactForm extends LightningElement {
+  handleSuccess() {
+    const inputFields = this.template.querySelectorAll('lightning-input-field');
+    if (inputFields) {
+      inputFields.forEach((field) => {
+        field.reset();
+      });
+    }
+    const toastEvent = new ShowToastEvent({
+      title: 'Submitted!',
+      message: 'Thank you for reaching out, I hope to get back to you shrotly.',
+      variant: 'success'
+    });
+    this.dispatchEvent(toastEvent);
+  }
+}
